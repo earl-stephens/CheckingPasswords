bf089d0f170a0767460e67571abe77cdf5232f80
earl-stephens <stephensearl@hotmail.com>
2021-12-19 20:54:39 -0700

    Completed coding challenge

--- src/application/App.java
+++ src/application/App.java
@@ -5,31 +5,42 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		
-		/* Coding challenge
-		 * Write a program that asks a user to input their password.
-		 * The program outputs "Password correct" or "Password
-		 * incorrect", after comparing the user input to the stored
-		 * password.
+
+		/*
+		 * Coding challenge Write a program that asks a user to input their password.
+		 * The program outputs "Password correct" or "Password incorrect", after
+		 * comparing the user input to the stored password.
+		 */
+
+		/*
+		 * Second coding challenge Make the program repeat the request for user input if
+		 * the user types the wrong password
 		 */
 
 		final String STORED_PASSWORD = "secure";
-		
+
 		System.out.println("Enter your password and hit return.");
-		
-		/* scanner itself isn't getting the input.  It's scanning
-		 * System.in for the input
+
+		/*
+		 * scanner itself isn't getting the input. It's scanning System.in for the input
 		 */
 		Scanner scanner = new Scanner(System.in);
-		String userInput = scanner.nextLine();
+		// String userInput = scanner.nextLine();
+
+		while (true) {
+
+			String userInput = scanner.nextLine();
+
+			if (userInput.equals(STORED_PASSWORD)) {
+				System.out.println("Password correct");
+				break;
+			} else {
+				System.out.println("Incorrect password.  Enter your password and hit return.");
+			}
+
+		}
 		scanner.close();
-		
-		if(userInput.equals(STORED_PASSWORD)) {
-			System.out.println("Password correct");
-		}
-		else {
-			System.out.println("Password incorrect");
-		}
+		System.out.println("Exiting.");
 	}
 
 }
